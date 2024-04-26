ac67b3a65e6456076ff55b274214530dc88fd9f5
PhatNV1 <phatnv@fsoft.vn>
2024-04-26 16:43:12 +0700

    Init

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,16 +1,20 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
+  password: `${process.env.POSTGRES_PASSWORD}`,
   database: process.env.POSTGRES_DB,
+  dbPort: Number(process.env.DB_PORT),
+  port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,11 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
+  port: config.dbPort,
 
   dialect: "postgres",
   storage: ":memory:",
